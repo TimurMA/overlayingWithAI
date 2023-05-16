c9fe6df3804e112a9bd70fe332f60bdd56ac7d4d
TimurMA <timur.minyazeff@gmail.com>
2023-05-16 17:44:34 +0300

    Доработал алгоритм вырезки

--- cutting_clothes.py
+++ cutting_clothes.py
@@ -21,21 +21,23 @@ colors_to_extract_top = [(47, 167, 239), (56, 140, 255), (75, 115, 255), (87, 24
                      (99, 94, 255), (125, 78, 255), (149, 67, 238), (167, 62, 210), (178, 60, 178)]
 
 
-def cut_top_clothes(image_path:str):
+def cut_top_clothes(image_path:str, output_mask_path:str, output_result_path:str):
     image = cv2.imread(image_path)
     colored_mask = predictColoredMask(image)
-    tf.keras.preprocessing.image.save_img("D:\Python37\Overlaying\output\colored_mask.png", colored_mask)
-    color_mask = cv2.imread("D:\Python37\Overlaying\output\colored_mask.png")
+    tf.keras.preprocessing.image.save_img(output_mask_path, colored_mask)
+    color_mask = cv2.imread(output_mask_path)
+
     new_mask = np.zeros(color_mask.shape[:2], dtype=np.uint8)
     for color in colors_to_extract_top:
         mask = cv2.inRange(color_mask, color, color)
         new_mask = cv2.bitwise_or(new_mask, mask)
+
     alpha_channel = new_mask.copy()
     alpha_channel[alpha_channel>0] = 255
     image = image.astype(np.uint8)
 
     result_cut = cv2.merge((image[:,:,0], image[:,:,1], image[:,:,2], alpha_channel))
-    cv2.imwrite("D:\Python37\Overlaying\output\cutted_clothes.png", result_cut)
+    cv2.imwrite(output_result_path, result_cut)
 
-cut_top_clothes("D:\Python37\Overlaying\output\\result.jpg")
+cut_top_clothes("D:\Python37\Overlaying\input\\6802df34.jpg", "D:\Python37\Overlaying\output\colored_mask.png", "D:\Python37\Overlaying\output\cutted_clothes.png")
 
