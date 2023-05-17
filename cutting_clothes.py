1f93c0b5e250ecceca97ddbf066502f0c91e2af2
TimurMA <timur.minyazeff@gmail.com>
2023-05-17 19:49:21 +0300

    доработал алгоритм вырезки

--- cutting_clothes.py
+++ cutting_clothes.py
@@ -21,8 +21,7 @@ colors_to_extract_top = [(47, 167, 239), (56, 140, 255), (75, 115, 255), (87, 24
                      (99, 94, 255), (125, 78, 255), (149, 67, 238), (167, 62, 210), (178, 60, 178), (169, 219, 28)]
 
 
-def cut_top_clothes(image_path:str, output_mask_path:str, output_result_path:str):
-    image = cv2.imread(image_path)
+def cut_top_clothes(image, image_keypoints, person_keypoints, output_mask_path:str, width, height):
     colored_mask = predictColoredMask(image)
     tf.keras.preprocessing.image.save_img(output_mask_path, colored_mask)
     color_mask = cv2.imread(output_mask_path)
@@ -35,6 +34,20 @@ def cut_top_clothes(image_path:str, output_mask_path:str, output_result_path:str
     alpha_channel = new_mask.copy()
     alpha_channel[alpha_channel>0] = 255
     image = image.astype(np.uint8)
-
     result_cut = cv2.merge((image[:,:,0], image[:,:,1], image[:,:,2], alpha_channel))
-    cv2.imwrite(output_result_path, result_cut)
+
+    
+    scaled_neck = image_keypoints[0][:2]
+    person1_neck = person_keypoints[0][:2]
+    x = int(person1_neck[0] - scaled_neck[0])
+    y = int(person1_neck[1] - scaled_neck[1])
+    bg_width = width
+    bg_height = height
+    if (y+result_cut.shape[0] > bg_height):
+        bg_height = y+result_cut.shape[0]
+    if (x+result_cut.shape[1] > bg_width):
+        bg_width = x+result_cut.shape[1]
+    transparent_bg = np.zeros((bg_height, bg_width, 4), dtype=np.uint8)
+    transparent_bg[:,:, 3] = 0
+    transparent_bg[y:y+result_cut.shape[0], x:x+result_cut.shape[1]] = result_cut
+    return transparent_bg[:height, :width]
