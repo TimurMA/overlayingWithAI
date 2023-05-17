9c3c0d1d3aefa65e12f4a276c60e13a3045343bb
TimurMA <timur.minyazeff@gmail.com>
2023-05-17 19:50:06 +0300

    закончил алгоритм накладывания

--- overlaying.py
+++ overlaying.py
@@ -4,7 +4,7 @@ from cutting_clothes import cut_top_clothes
 import numpy as np
 import sys
 from propotions import scale_image
-#include 
+
 
 sys.path.append('D:\\Python37\\openpose\\build\\python\\openpose\\Release')
 import pyopenpose as op
@@ -37,29 +37,23 @@ opWrapper.emplaceAndPop(op.VectorDatum([datum]))
 person1_keypoints = datum.poseKeypoints[0][1:9]
 
 
-target_distance_ox = abs(person1_keypoints[1][0]-person1_keypoints[4][0])
-target_distance_oy = abs(person1_keypoints[0][1]-person1_keypoints[-1][1])
+target_distance_ox = person1_keypoints[1][0]-person1_keypoints[4][0]
+target_distance_oy = person1_keypoints[0][1]-person1_keypoints[-1][1]
 
-scaled_image = scale_image(person2, person2_keypoints, target_distance_ox, target_distance_oy)
-# cv2.imwrite('D:\Python37\Overlaying\output\\result.jpg', scaled_image)
-# cut_top_clothes('D:\Python37\Overlaying\output\\result.jpg', 'D:\Python37\Overlaying\output\colored_mask.png', 
-#                 'D:\Python37\Overlaying\output\cutted_clothes.png')
+scaled_image, scaled_keypoints = scale_image(person2, person2_keypoints, target_distance_ox, target_distance_oy)
+cutted_image = cut_top_clothes(scaled_image, scaled_keypoints, person1_keypoints,
+                 'D:\Python37\Overlaying\output\colored_mask.png', person1.shape[1], person1.shape[0])
 
-datum = op.Datum()
-datum.cvInputData = scaled_image
-opWrapper.emplaceAndPop(op.VectorDatum([datum]))
-scaled_keypoints = datum.poseKeypoints[0][1:9]
+alpha = cutted_image[:,:, 3] / 255.0
+cutted_image = cutted_image[:,:,:3]
+cutted_image = cv2.cvtColor(cutted_image, cv2.COLOR_RGBA2RGB)
 
-dx = scaled_keypoints[4][0] - scaled_keypoints[1][0]
-dy = scaled_keypoints[4][1] - scaled_keypoints[1][1]
-scaled_neck = scaled_keypoints[0][:2]
-person1_neck = person1_keypoints[0][:2]
-
-x = int(scaled_neck[0] - person1_neck[0] + (person1.shape[1] - dx)/2)
-y = int(scaled_neck[1] - person1_neck[1] + (person1.shape[0] - dy)/2)
-
-person1[y:y+scaled_image.shape[0],x:x+scaled_image.shape[1]] = scaled_image
-cv2.imshow('result', person1)
+person = cv2.imread('D:\Python37\Overlaying\input\input.jpg')
 
 
+composite = cv2.bitwise_and(cutted_image, cutted_image, mask = (alpha > 0).astype(np.uint8))
+backround_masked = cv2.bitwise_and(person, person, mask = (1 - alpha >0).astype(np.uint8))
 
+result = cv2.add(composite, backround_masked)
+result_path = 'D:\Python37\Overlaying\output\output2.jpg'
+cv2.imwrite(result_path, result)
