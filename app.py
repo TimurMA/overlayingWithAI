8d41bd8b7aac4d04c24d5c0f6ddd6c8c9ee8d110
TimurMA <timur.minyazeff@gmail.com>
2023-05-21 18:54:42 +0300

    API server

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from flask_restful import Api, Resource
 from overlaying import overlay
-from cv2 import imwrite
 from json import dumps, loads
-
+from cv2 import imwrite
 app = Flask(__name__)
 api = Api(app)
 
 class API(Resource):
-    def post(image):
-        output_path = 'D:\Python37\Overlaying\VAISoutput/output.jpg'
-        imwrite(output_path, overlay(image))
+    def post(image_URL):
+
+        output_path = 'D:\Python37\Overlaying\output\output.jpg'
+        imwrite(output_path, overlay(image_URL))
 
         return loads(dumps({
             'name': 'AIPic',
