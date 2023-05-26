b19d85f9374336b6090f067fb691087bece0cc4d
TimurMA <timur.minyazeff@gmail.com>
2023-05-26 16:09:34 +0300

    отправка выходных данных

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
-from flask import Flask, request
-from flask_restful import Api, Resource
+from flask import Flask, request, send_file
+from flask_restful import Api
 from overlaying import overlay
-from json import dumps, loads
-from cv2 import imwrite
-from os import getcwd
+from cv2 import imwrite, imdecode, IMREAD_COLOR
+from urllib.request import urlopen
+from numpy import asarray
+from PIL import Image
+import io
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/get-cut-clothes', methods = ['POST']) 
+output_path = 'output/output.jpg'
+@app.route('/output.jpg', methods = ['POST']) 
 def post(): 
     data = request.get_data().decode('ascii')
-    output_path = 'output\output.jpg'
-    imwrite(output_path, overlay(data))
+    input_path = 'input\input.jpg'
+    url = urlopen(data)
+    per_array = asarray(bytearray(url.read()), dtype = 'uint8')
+    person = imdecode(per_array, IMREAD_COLOR)
+    imwrite(input_path, person)
+    overlay()
 
-    return loads(dumps({
-            'name': 'AIPic',
-            'type': 'image/jpg',
-            'uri': getcwd() + '\\' + output_path
-        }))
+    file_object = io.BytesIO()
+    img = Image.open(output_path)
+    img.save(file_object, 'JPG')
+    file_object.seek(0)
+    return send_file(file_object, mimetype='image/JPG')
+
 
 if __name__ == "__main__": 
     app.run(debug=True)
